6530005d816844dd24f0ecf0fba5d361b636af14
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-12 20:08:52 -0800

    finishing services components

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -11,3 +11,24 @@ export const socialLinks = [
   { id: 3, href: 'https://ca.linkedin.com/', icon: 'linkedin' },
   { id: 4, href: 'https://www.squarespace.com/', icon: 'squarespace' },
 ]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'bolt',
+    title: 'fast and responsive',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'desktop',
+    title: 'SEO and accessibility',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fingerprint',
+    title: 'advanced web security',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
