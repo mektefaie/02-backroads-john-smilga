1a197260eb17f3657bef3f872500ef510cafab95
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-12 20:31:07 -0800

    finishing featured website

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,3 +1,10 @@
+import tour1 from '../images/tour-1.jpeg'
+import tour2 from '../images/tour-2.jpeg'
+import tour3 from '../images/tour-3.jpeg'
+import tour4 from '../images/tour-4.jpeg'
+import tour5 from '../images/tour-5.jpeg'
+import tour6 from '../images/tour-6.jpeg'
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -30,5 +37,68 @@ export const services = [
     icon: 'fingerprint',
     title: 'advanced web security',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2023',
+    title: 'tibet adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    price: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'august 26th, 2023',
+    title: 'tibet adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    price: 2100,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'august 26th, 2023',
+    title: 'tibet adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    price: 2100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'august 26th, 2023',
+    title: 'tibet adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    price: 2100,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: 'august 26th, 2023',
+    title: 'tibet adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    price: 2100,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: 'august 26th, 2023',
+    title: 'tibet adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    price: 2100,
   },
 ]
